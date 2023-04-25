fa4a1f7673a16dd0ffaf96928dc308439c7cc7a8
Ayush Shukla <shuklaayush247@gmail.com>
2023-04-25 14:33:31 +0200

    feat: reimplement fields here

--- arithmetic/curves/src/ed25519/mod.rs
+++ arithmetic/curves/src/ed25519/mod.rs
@@ -1,1 +1,9 @@
-pub use ed25519::{SubgroupPointAffine as Ed25519Affine, Fq, Fr};
+#[macro_use]
+mod util;
+mod fq;
+mod fr;
+
+pub use fq::*;
+pub use fr::*;
+
+pub use ed25519::{SubgroupPointAffine as Ed25519Affine};
